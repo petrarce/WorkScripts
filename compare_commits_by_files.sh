e3d5d6be3ddb06edd0fdd43749d66425b1ebba9b
ivan <petrarce@gmail.com>
2016-10-09 12:26:57 +0300

    - correctly working "files structure creation" - correctly working "files structure analiser"

--- compare_commits_by_files.sh
+++ compare_commits_by_files.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#set -x
+##set -x
+#================dirs structure================
 TOPDIR=`pwd`
+WORKDIR=${TOPDIR}/files
+
 CC_int_2_0ChecksumsFile='2_0_CC_int.checksums'
 int_2_0ChecksumsFile='2_0_int.checksums'
 natchesFile='cc_int-int.matches'
@@ -19,11 +22,83 @@ int_2_0Commits=`cat ${TOPDIR}/${reposWithCommits_int_2_0File}`
 
 branchCC_int2_0='D3.1_GW-SDK2.0_CC_Int'
 branchInt2_0='D3.1_GW-SDK2.0_int'
+
+tempCommotFile="temp"
 #generateChecksumsFile
 #Args:
 #	inpFileWithCommits file where stored commits and their relative repos
 #	outpFileWithCommits - file where wi will putt checksums
 #	branchName - branch on which we are working
+createFolderStructure(){
+	dataFile=`cat ${1}`
+	branchname=${2}
+	repoInfoFile=""
+
+	mkdir -p ${WORKDIR}/${branchname}
+	for i in ${dataFile}; do
+		if [[ ! -z "$(echo ${i} | grep ":" )" ]]; then
+			TEMP_WORKDIR=${WORKDIR}/${branchname}/$(echo ${i} | sed -e 's,:,,g')
+			mkdir -p ${TEMP_WORKDIR}; cd ${TEMP_WORKDIR}
+			repoInfoFile="$(echo ${i} | sed -e 's,:,,g')-info"
+			echo "${i}" > ${TEMP_WORKDIR}/repoInfoFile
+		else
+			echo "${i}" >> ${TEMP_WORKDIR}/repoInfoFile
+		fi
+	done
+}
+
+readCommit(){
+	commit=${1}
+	git diff ${1}
+}
+
+analiseDir(){
+	local ARG=${1}
+	local makeInDir=${2}
+	local ARGS=${3}
+
+	FILES_LIST=`ls -l | awk '{print $9}'`
+	for i in ${FILES_LIST}; do
+		echo ${CUR_DIR}/${i} =================
+		echo ${ARG} $makeInDir $ARGS
+		${makeInDir} ${i} ${ARGS}
+		sleep 1
+	done
+}
+
+analiseRepoDir(){
+	local CUR_DIR=`pwd`
+
+	cd ${CUR_DIR}/${1}
+	echo '>>>>>>>REPO' ${CUR_DIR}/${1}
+	analiseDir NULL "cat" ""
+	echo '<<<<<<<REPO' ${CUR_DIR}/
+
+	cd ${CUR_DIR}/
+}
+
+analiseBranchDir(){
+	local CUR_DIR=`pwd`
+
+	cd ${CUR_DIR}/${1}
+	echo '>>>>>>>BRANCHDIR' ${CUR_DIR}/${1}
+	analiseDir NULL analiseRepoDir ""
+	echo '<<<<<<<BRANCHDIR' ${CUR_DIR}/
+	cd ${CUR_DIR}/
+}
+
+analiseWorkDir(){
+	local CUR_DIR=`pwd`
+
+	cd ${CUR_DIR}/${1}
+	echo '>>>>>>>WORKDIR' ${CUR_DIR}/${1}
+
+	analiseDir NULL analiseBranchDir ""
+	echo '<<<<<<<WORKDIR' ${CUR_DIR}/
+
+	cd ${CUR_DIR}/
+}
+
 generateChecksumsFile(){
 		inpFileWithCommits=${1}
 		outpFileWithCommits=${2}
@@ -57,24 +132,15 @@ grepChecksumm(){
 }
 
 
+
 #==================Main script==================
 
 #cat ${TOPDIR}/${int_2_0CommitsFiles} | awk '{print $1}' | \
 #	grep -ve "Commits" -ve  "------------"
 
-#clear all files
-echo > ${TOPDIR}/${CC_int_2_0ChecksumsFile}
-echo > ${TOPDIR}/${int_2_0ChecksumsFile}
 
-generateChecksumsFile ${reposWithCommitsCC_int_2_0File} \
-											${CC_int_2_0ChecksumsFile}				\
-											${branchCC_int2_0}
-generateChecksumsFile ${reposWithCommits_int_2_0File} \
-											${int_2_0ChecksumsFile}				\
-											${branchInt2_0}
-grepChecksumm ${CC_int_2_0ChecksumsFile} ${int_2_0ChecksumsFile}
+createFolderStructure ${TOPDIR}/commits_fileCC_int.txt ${branchCC_int2_0}
+createFolderStructure ${TOPDIR}/commits_file_int.txt ${branchInt2_0}
 
 cd ${TOPDIR}
-set +x
-
-#echo "${CC_int_2_0Commits}"
+analiseWorkDir files
