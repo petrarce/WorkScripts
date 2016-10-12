8cccaa6c7876a79d081cff033c526a80cd0307d6
Ivan <i.foljbort@inango-systems.com>
2016-10-12 16:44:35 +0300

    nightly build has been debugged - Creating dir SUCCEED" - passed - Starting: repo init SUCCEED - passed - Starting: repo sync SUCCEED - passed - Starting: setup-environment - passed - Starting: bitbake rdk-generic-broadband-image - passed added header

--- nightly_build.sh
+++ nightly_build.sh
@@ -1,59 +1,70 @@
+################################################################################
+#This script creates fetches new environment with repo and builds images with
+#of machines, specified in NIGHTLY_BUILD_MACHINES variable in global environment
+#(should be set on terminal)
+#
+#execute this script from source:
+#. nightly_build opt1 opt2 opt3
+#
+#opt1 - comcast directory, where all repo views are
+#opt2 - current repo view (optional)
+#opt3 - skip sync process (if repo view already exists and configured /optional)
+#
+# developers - Konstantin Fursenko, Ivan Folbort
+################################################################################
 #!/bin/bash
-set -x
 #==================VARIABLES=================
 #-----Generic
 TOPDIR=${1}
+if [ -z "${2}" ]; then WORKDIR=${TOPDIR}/xb6_comcast_bugtest_$(date +%d-%m-%Y_%H-%M-%S)
+                  else WORKDIR=${TOPDIR}/${2} ;fi
 DL_DIR=${TOPDIR}/downloads
-WORKDIR=${TOPDIR}/xb6_comcast_bugtest_$(date +%d-%m-%Y_%H-%M-%S)
 logFile=${WORKDIR}/setup.log
+ARG=${3}
 
-#----DEBUG/TEMPORARY BLOCK
-WORKDIR=/local/i.foljbort/work/comcast/xb6_comcast_bugtest_10-10-2016_18-19-17
-logFile=${WORKDIR}/setup.log
-#----
-
+configure_builddir(){
+    sed -ie 's,BB_NUMBER_THREADS ?= "${@oe.utils.cpu_count()}",BB_NUMBER_THREADS ?= "8",g' \
+        conf/local.conf
+}
+#==============MAIN==============
 [ ! -d ${TOPDIR} ] && echo 'Top directory specified incorrectly'
 [ ! -d ${DL_DIR} ] && echo 'Downloads path is not exists'
 
-echo "Creating dir: \"${WORKDIR}\""
-mkdir "${WORKDIR}"
+echo "Creating dir: \"${WORKDIR}\"\n ${DL_DIR} \n download symlink"
+mkdir "${WORKDIR}"; mkdir ${DL_DIR}
+ln -s ${TOPDIR}/downloads ${WORKDIR}/downloads
 cd "${WORKDIR}"
-echo "lgfile">${logFile}
 
-#echo "DEBUG: Creating dir SUCCEED" && return
+if [[ ! "${ARG}" = "--no-sync" ]]; then
+    #statements
 
 echo "Starting: repo init"
 repo init -u ssh://gerrit.teamccp.com:29418/rdk/yocto_oe/manifests/arris-intel-manifest \
           -b master -m arrisxb6.xml --repo-url=ssh://gerrit.teamccp.com:29418/rdk/tools/git-repo \
-          --no-repo-verify -g all >>${logFile} 2>&1
-
-#echo "DEBUG: Starting: repo init SUCCEED" && return
+          --no-repo-verify -g all
 
 echo "Starting: repo sync"
-repo sync --verify >>${logFile} 2>&1
+repo sync --verify
 
-#echo "DEBUG: Starting: repo sync SUCCEED" && return
+fi
 
 cd ${WORKDIR}
 for MACHINE in ${NIGHTLY_BUILD_MACHINES}; do
   echo "Starting: setup-environment for ${MACHINE}"
-  . meta-rdk/setup-environment >>${logFile} 2>&1
+  . meta-rdk/setup-environment ${WORKDIR}/build-${MACHINE}
+  configure_builddir
   cd ${WORKDIR}
 done
 
-set +x
-echo "DEBUG: Starting: Starting: setup-environment SUCCEED" && return
-
 #prepare build-${Machines} directories list to enter them and start builds
-BUILD_LIST=${WORKDIR}/$(find ${WORKDIR} -name build*)
+BUILD_LIST="$(find ${WORKDIR} -maxdepth 1 -name build\*)"
 
 for BUILD_DIR in ${BUILD_LIST}; do
   cd ${BUILD_DIR}
   echo "Starting: bitbake rdk-generic-broadband-image"
-  bitbake rdk-generic-broadband-image #>>${BUILD_DIR}/build.log 2>&1
+  bitbake rdk-generic-broadband-image
 done
 
-echo "DEBUG: Started: bitbake rdk-generic-broadband-image SUCCEED" && return
+cd ${TOPDIR}
 
-cd ${WORKDIR}
 echo "Finished"
